acf4ef9ad51e6f4a800a91389cd7c6e2abd0ab1a
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2013-09-03 13:00:38 +0300

    removed config.php for git deployment

--- includes/config-sample.php
+++ includes/config-sample.php
@@ -9,18 +9,9 @@ global $lb_root;
 
 $lb_root = __DIR__;
 
-$db_username = "mustapha";
-$db_password = "mm000741";
+$db_username = "username";
+$db_password = "password";
 $db_host ="localhost";
-$db_database="lebanese_blogs";
-
-/* for online use
-
-$db_username = "beirut7_lbblogs";
-$db_password = "mm000741";
-$db_host ="localhost";
-$db_database="beirut7_lebanese_blogs_database";
-
-*/
+$db_database="database name";
 
  ?>
