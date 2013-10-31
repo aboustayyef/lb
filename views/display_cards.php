577b6d746560cbd7ce38d6266b2ddd6f3056e507
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2013-10-31 22:27:46 +0000

    worked on posts sharing tools. Accomodated save post option

--- views/display_cards.php
+++ views/display_cards.php
@@ -52,18 +52,16 @@
 			</div>
 
 			<!--card footer-->
-				<div class ="sharing">
-					<div class="card_footer">
+					<div class="card_footer nopadding">
 						<?php 
 						$tweetcredit = ($post['blog_author_twitter_username'])?" by @{$post['blog_author_twitter_username']}":"";
 						$url_to_incode = "{$post['post_title']} {$post['post_url']}".$tweetcredit." via lebaneseblogs.com";
 						$twitterUrl = urlencode($url_to_incode);
 	 				?>
-					<div class ="sharing_tools">
 						<ul>
-							<li> <a href="https://twitter.com/intent/tweet?text=<?php echo $twitterUrl; ?>" title="Click to send this post to Twitter!" target="_blank"><i class="icon-twitter-sign icon-large"></i> Tweet</a> </li>
-							<li> <a href="http://www.facebook.com/sharer.php?u='.$post_url.'"><i class="icon-facebook-sign icon-large"></i> Share</a> </li>
-							<li> <a href="./<?php echo $post['blog_id'] ?>"><i class="icon-question-sign icon-large"></i> About this blog</a> </li>
+							<li class="save"><a href="#"><i class="icon-heart"></i> Save Post</a></li>
+							<li> <a href="https://twitter.com/intent/tweet?text=<?php echo $twitterUrl; ?>" title="Click to send this post to Twitter!" target="_blank"><i class="icon-twitter icon-large"></i> Tweet</a> </li>
+							<li> <a href="http://www.facebook.com/sharer.php?u='.$post_url.'"><i class="icon-facebook icon-large"></i> Share</a> </li>
 					<?php 
 						/*if (admin_logged_in()) {
 							echo "<li>$post_visits</li>";
@@ -71,11 +69,9 @@
 						}*/
 					?>
 						</ul>
-					</div>
 
 
 					</div>
-				</div>
 			</div> <!-- /content module -->
 	    
 			<?php
