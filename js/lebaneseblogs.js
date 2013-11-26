9d058499962a97e249328168faf704bc866b14d2
Mustapha <mustapha.hamoui@gmail.com>
2013-11-26 23:04:28 +0000

    Saved page will now refresh if items were removed from it

--- js/lebaneseblogs.js
+++ js/lebaneseblogs.js
@@ -89,6 +89,10 @@ var lbApp ={
 								}
 							//$(this).html("<b>this</b> is a test");
 					});
+					if (global_page === "saved") {
+						// if we are in the favorites page, refresh page to reflect changes
+						location.reload();
+					};
 				});
 			}
 		},
