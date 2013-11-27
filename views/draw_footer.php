c20ebd3e91ee7d2e36661d680e914d7d99b6fc19
Mustapha <mustapha.hamoui@gmail.com>
2013-11-27 21:43:48 +0000

    started using compressed javascript and redeployed google analytics

--- views/draw_footer.php
+++ views/draw_footer.php
@@ -12,7 +12,7 @@
     <!--<script src="<?php echo WEBPATH ;?>js/jquery.lazyload.min.js"></script>-->
 
     <!-- App JS -->
-    <script src="<?php echo WEBPATH ;?>js/lebaneseblogs.js?<?php echo time(); ?>"></script>
+    <script src="<?php echo WEBPATH ;?>js/lebaneseblogs-ck.js?<?php echo time(); ?>"></script>
 
     <!-- Start of StatCounter Code for Default Guide -->
                 <script type="text/javascript">
@@ -31,7 +31,7 @@
                 alt="web counter"></a></div></noscript>
             <!-- End of StatCounter Code for Default Guide -->
 
-            <!-- Start of Google Analytics Code 
+            Start of Google Analytics Code 
                 <script>
                     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -40,6 +40,6 @@
                     ga('create', 'UA-40418714-1', 'lebaneseblogs.com');
                     ga('send', 'pageview');
                 </script>
-    -->
+   
   </body>
 </html>
