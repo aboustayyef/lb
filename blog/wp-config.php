26a983dd03918f3daf228876b100a25f1dcfa38b
Digital Ocean Server <mustapha.hamoui@gmail.com>
2013-11-28 06:56:33 +0000

    changes made in server and an blog brought back to github

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'meta_blog_data');
+define('DB_NAME', 'metablog_lebanese_blogs');
 
 /** MySQL database username */
 define('DB_USER', 'mustapha');
