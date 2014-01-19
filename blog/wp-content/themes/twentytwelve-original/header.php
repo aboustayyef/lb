8664b09341931356fbf6f35a2030bd7905639640
Digital Ocean Server <mustapha.hamoui@gmail.com>
2014-01-19 20:18:15 +0000

    modified header on blog to include new logo

--- blog/wp-content/themes/twentytwelve-original/header.php
+++ blog/wp-content/themes/twentytwelve-original/header.php
@@ -35,7 +35,7 @@
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
-			<a href ="http://lebaneseblogs.com"><img src ="http://lebaneseblogs.com/blog/wp-content/uploads/2013/04/logo_new_360x60x2.png" width ="300" ></a>
+			<a href ="http://lebaneseblogs.com"><img src ="http://lebaneseblogs.com/blog/lb-blog-masthead.png" width ="169" height="72" ></a>
 			<!-- <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> -->
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
